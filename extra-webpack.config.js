acc059c508fb24bd162706443aef06d9a0a61706
Matthieu Drula <matthieu.drula.hs@gmail.com>
2018-08-15 14:46:26 +0200

    add vue webpack config

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,6 +1,29 @@
 console.log('The custom config is used');
 
+const vueAlias = {
+  extensions: [".ts", ".js", ".vue", ".json"],
+  alias: {
+    vue$: "vue/dist/vue.esm.js"
+  }
+};
+
+const vueRule = [{
+  test: /\.vue$/,
+  loader: "vue-loader",
+  options: {
+    // Since sass-loader (weirdly) has SCSS as its default parse mode, we map the
+    // "scss" and "sass" values for the lang attribute to the right configs here.
+    // other preprocessors should work out of the box, no loader config like this
+    // necessary.
+    scss: ["vue-style-loader", "css-loader", "sass-loader"],
+    sass: ["vue-style-loader", "css-loader", "sass-loader?indentedSyntax"]
+  }
+  // other vue-loader options go here
+}];
+
 module.exports = {
-  resolve: {},
-  module: {}
+  resolve: vueAlias,
+  module: {
+    rules: vueRule
+  }
 }
